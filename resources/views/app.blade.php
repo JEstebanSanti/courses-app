2527539ebf0d0e94bc4991b8909e3d1edbc8cb81
JEstebanSanti <a20311083@uthermosillo.edu.mx>
2024-05-05 23:13:19 -0700

    adds csrf token meta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
