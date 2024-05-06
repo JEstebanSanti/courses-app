e5d5eedc47c15ff30b835e80c792101864610f05
JEstebanSanti <a20311083@uthermosillo.edu.mx>
2024-05-05 23:20:44 -0700

    adds csrf axios ajax config boostrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,8 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
